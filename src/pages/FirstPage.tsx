7a3d28f253711fcd6bc317d491e417deae1b717b
Skeller1z <kaktv1333@gmail.com>
2023-06-22 13:42:05 +0700

    first commit

---
+++ src/pages/FirstPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function FirstPage() {
+  return (
+    <div>FirstPage</div>
+  )
+}
+
+export default FirstPage
---
+++ src/routes/routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+
+
+export default function Router() {
+    return ([
+        {
+
+        }
+    ])
+}
+
